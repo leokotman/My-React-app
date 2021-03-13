777641d182c7887dcd5be03b5476d10a0fdde127
levkotman <leoktmn@yandex.ru>
2021-03-13 20:23:28 +0100

    state, functions – to store (OOP)
    store: { state, methods }

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,62 +1,63 @@
-let rerenderEntireTree = () => {
-  console.log("state is changed");
-};
-
-let state = {
-  profilePage: {
-    posts: [
-      {id: 1, message: "Hey how are you?", likes: 50, views: 200},
-      {id: 2, message: "It's my second post", likes: 30, views: 100},
-      {id: 3, message: "Yo what's up guys?", likes: 60, views: 300},
-    ],
-    newText: "LeoKot",
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        {id: 1, message: "Hey how are you?", likes: 50, views: 200},
+        {id: 2, message: "It's my second post", likes: 30, views: 100},
+        {id: 3, message: "Yo what's up guys?", likes: 60, views: 300},
+      ],
+      newText: "LeoKot",
+    },
+    dialogsPage: {
+      dialogs: [
+        {id: 1, name: "Leo"},
+        {id: 2, name: "Jes"},
+        {id: 3, name: "Mum"},
+        {id: 4, name: "Gra"},
+        {id: 5, name: "Alex"},
+      ],
+      messages: [
+        {id: 1, message: "Hi"},
+        {id: 2, message: "How are you?"},
+        {id: 3, message: "Let's meet tomorrow, bro"},
+      ],
+    },
+    sidebar: {
+      friends: [
+        {id: 1, name: "Leo", img: "img1"},
+        {id: 2, name: "Jes", img: "img2"},
+        {id: 3, name: "Mum", img: "img3"},
+        {id: 4, name: "Gra", img: "img4"},
+        {id: 5, name: "Alex", img: "img5"},
+      ],
+    },
   },
-  dialogsPage: {
-    dialogs: [
-      {id: 1, name: "Leo"},
-      {id: 2, name: "Jes"},
-      {id: 3, name: "Mum"},
-      {id: 4, name: "Gra"},
-      {id: 5, name: "Alex"},
-    ],
-    messages: [
-      {id: 1, message: "Hi"},
-      {id: 2, message: "How are you?"},
-      {id: 3, message: "Let's meet tomorrow, bro"},
-    ],
+  getState () {
+    return this._state;
   },
-  sidebar: {
-    friends: [
-      {id: 1, name: "Leo", img: "img1"},
-      {id: 2, name: "Jes", img: "img2"},
-      {id: 3, name: "Mum", img: "img3"},
-      {id: 4, name: "Gra", img: "img4"},
-      {id: 5, name: "Alex", img: "img5"},
-    ],
+  _rerenderEntireTree () {
+    console.log("state is changed");
   },
-};
+  addPost () {
+    let newPost = {
+      id: this._state.profilePage.posts.length,
+      message: this._state.profilePage.newText,
+      likes: 0,
+      views: 0,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newText = ""; //обнулили окно ввода теперь здесь в BLL
+    this._rerenderEntireTree(this._state);
+  },
+  updateNewPostText (newText) {
+    this._state.profilePage.newText = newText;
+    this._rerenderEntireTree(this._state);
+  },
+  subscribe (observer) {
+    this._rerenderEntireTree = observer;
+  }
+}
 
-window.state = state;
+window.store = store;
 
-export const addPost = () => {
-  let newPost = {
-    id: state.profilePage.posts.length,
-    message: state.profilePage.newText,
-    likes: 0,
-    views: 0,
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newText = ""; //обнулили окно ввода теперь здесь в BLL
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newText = newText;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
